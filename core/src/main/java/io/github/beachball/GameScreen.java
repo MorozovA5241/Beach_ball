41db947e8c0eb723e3554b80966b27f504962678
MorozovA5241 <s79227499306@gmail.com>
2025-05-01 13:10:14 +0500

    assets

--- core/src/main/java/io/github/beachball/GameScreen.java
+++ core/src/main/java/io/github/beachball/GameScreen.java
@@ -5,16 +5,19 @@ import static io.github.beachball.GameSettings.BALL_BIT;
 import static io.github.beachball.GameSettings.BALL_IMAGE_PATH;
 import static io.github.beachball.GameSettings.BUTTON_IMG_PATH;
 import static io.github.beachball.GameSettings.FLOOR_BIT;
+import static io.github.beachball.GameSettings.LEFTBUTTON_IMAGE_PATH;
 import static io.github.beachball.GameSettings.OBJECT_HEIGHT;
 import static io.github.beachball.GameSettings.OBJECT_IMG_PATH;
 import static io.github.beachball.GameSettings.OBJECT_WIDTH;
 import static io.github.beachball.GameSettings.PLAYER_BIT;
 import static io.github.beachball.GameSettings.PLAYER_IMAGE_PATH;
+import static io.github.beachball.GameSettings.RIGHTBUTTON_IMAGE_PATH;
 import static io.github.beachball.GameSettings.SCALE;
 import static io.github.beachball.GameSettings.SCREEN_HEIGHT;
 import static io.github.beachball.GameSettings.SCREEN_WIDTH;
 import static io.github.beachball.GameSettings.OBJECT_IMG_PATH;
 import static io.github.beachball.GameSettings.SIMPLE_BIT;
+import static io.github.beachball.GameSettings.UPBUTTON_IMAGE_PATH;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
@@ -52,14 +55,14 @@ public class GameScreen extends ScreenAdapter {
 
         gameObject = new GameObject(SCREEN_WIDTH / 2 - 200, 200, 200, 200 , main.world, PLAYER_IMAGE_PATH, PLAYER_BIT, 1f, 1f, 0f); // делаем динамический объект
         box = new StaticGameObject(SCREEN_WIDTH, 10, OBJECT_WIDTH+ 100000, OBJECT_HEIGHT - 80, main.world, OBJECT_IMG_PATH, FLOOR_BIT);// делаем статический объект
-        rightButton = new ButtonView(20, 100, 100, 100, OBJECT_IMG_PATH);
-        jumpButton = new ButtonView(1100, 100, 100, 100, OBJECT_IMG_PATH);
-        leftButton = new ButtonView(140, 100, 100, 100, OBJECT_IMG_PATH);
+        rightButton = new ButtonView(20, 100, 100, 100, LEFTBUTTON_IMAGE_PATH);
+        jumpButton = new ButtonView(1100, 100, 100, 100, UPBUTTON_IMAGE_PATH);
+        leftButton = new ButtonView(140, 100, 100, 100, RIGHTBUTTON_IMAGE_PATH);
         wall = new StaticGameObject(SCREEN_WIDTH/2, 30, 20, 600, main.world, OBJECT_IMG_PATH, SIMPLE_BIT);
-        ball = new GameObject(SCREEN_WIDTH/2 - 100, 600, 100, 100, main.world, BALL_IMAGE_PATH, BALL_BIT, 1.5f, 1.5f, 1.4f);
-        topSideWall = new StaticGameObject(SCREEN_WIDTH / 2, SCREEN_HEIGHT, 3000, 80, main.world, OBJECT_IMG_PATH, SIMPLE_BIT); //под вопросом
-        rightSideWall = new StaticGameObject(0, SCREEN_HEIGHT / 2, 3, 1500, main.world, OBJECT_IMG_PATH, SIMPLE_BIT);
-        leftSideWall = new StaticGameObject(SCREEN_WIDTH, SCREEN_HEIGHT / 2, 3, 1500, main.world, OBJECT_IMG_PATH, SIMPLE_BIT);
+        ball = new GameObject(SCREEN_WIDTH/2 - 100, 600, 100, 100, main.world, BALL_IMAGE_PATH, BALL_BIT, 1.0f, 1.0f, 1.4f);
+        //topSideWall = new StaticGameObject(SCREEN_WIDTH / 2, SCREEN_HEIGHT, 3000, 80, main.world, OBJECT_IMG_PATH, SIMPLE_BIT); //под вопросом
+        rightSideWall = new StaticGameObject(0, SCREEN_HEIGHT / 2, 3, 5500, main.world, OBJECT_IMG_PATH, SIMPLE_BIT);
+        leftSideWall = new StaticGameObject(SCREEN_WIDTH, SCREEN_HEIGHT / 2, 3, 5500, main.world, OBJECT_IMG_PATH, SIMPLE_BIT);
         baffle = new StaticGameObject(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 20, 1500, main.world, OBJECT_IMG_PATH, BAFFLE_BIT);
     }
 
@@ -78,7 +81,7 @@ public class GameScreen extends ScreenAdapter {
         box.draw(main.batch);
         wall.draw(main.batch);
         ball.draw(main.batch);
-        topSideWall.draw(main.batch);
+       // topSideWall.draw(main.batch);
         rightSideWall.draw(main.batch);
         leftSideWall.draw(main.batch);
         main.batch.end(); // рендер(прорисовка кадра)
@@ -86,7 +89,7 @@ public class GameScreen extends ScreenAdapter {
 
         if(ball.needSetPosition == true){  // жесточайший костыль для телепортации мячика убиваем старый и создаем другой
             main.world.destroyBody(ball.body);
-            ball = new GameObject(SCREEN_WIDTH/2 - 100, 500, 100, 100, main.world, BALL_IMAGE_PATH, BALL_BIT, 1.5f, 1.5f, 1.4f);
+            ball = new GameObject(SCREEN_WIDTH/2 - 100, 500, 100, 100, main.world, BALL_IMAGE_PATH, BALL_BIT, 1.0f, 1.0f, 1.4f);
         }
 
         handleMovementInput();
